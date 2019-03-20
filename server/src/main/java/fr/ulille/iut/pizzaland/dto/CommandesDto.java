623aba7eb7ba5ebe0d74844ef7333ce51452f80f
BLOCH Sacha <sachabloch@gmail.com>
2019-03-20 14:55:55 +0100

    UtilisateursDto and CommandesDto finish

---
+++ server/src/main/java/fr/ulille/iut/pizzaland/dto/CommandesDto.java
@@ -0,0 +1,56 @@
+package fr.ulille.iut.pizzaland.dto;
+
+import java.sql.Date;
+
+/**
+ * CommandesDto
+ *
+ * @author blochs
+ */
+public class CommandesDto {
+    private int idCommande;
+    private int idUtilisateur;
+    private double prix;
+    private Date date;
+    private Date dateArrive;
+
+    public int getIdCommande() {
+        return idCommande;
+    }
+
+    public void setIdCommande(int idCommande) {
+        this.idCommande = idCommande;
+    }
+
+    public int getIdUtilisateur() {
+        return idUtilisateur;
+    }
+
+    public void setIdUtilisateur(int idUtilisateur) {
+        this.idUtilisateur = idUtilisateur;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getDateArrive() {
+        return dateArrive;
+    }
+
+    public void setDateArrive(Date dateArrive) {
+        this.dateArrive = dateArrive;
+    }
+}
---
+++ server/src/main/java/fr/ulille/iut/pizzaland/dto/UtilisateursDto.java
@@ -0,0 +1,81 @@
+package fr.ulille.iut.pizzaland.dto;
+
+/**
+ * UtilisateursDto
+ *
+ * @author blochs
+ */
+public class UtilisateursDto {
+    private int idUtilisateur;
+    private String pseudo;
+    private String mdp;
+    private String sel;
+    private String prenom;
+    private String nom;
+    private String adresse;
+    private String adresseMail;
+
+    public int getIdUtilisateur() {
+        return idUtilisateur;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public String getSel() {
+        return sel;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String getAdresseMail() {
+        return adresseMail;
+    }
+
+    public void setIdUtilisateur(int idUtilisateur) {
+        this.idUtilisateur = idUtilisateur;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public void setSel(String sel) {
+        this.sel = sel;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public void setAdresseMail(String adresseMail) {
+        this.adresseMail = adresseMail;
+    }
+}
