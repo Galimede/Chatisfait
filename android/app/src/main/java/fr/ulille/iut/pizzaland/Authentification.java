9d14db8b3efc40af07ec5656ac395eeae021181e
Zinedine ZERROUKI <zinedine.zerrouki.etu@univ-lille.fr>
2019-03-21 11:13:32 +0100

    Authentification sur mobile OK

--- android/app/src/main/java/fr/ulille/iut/pizzaland/Authentification.java
+++ android/app/src/main/java/fr/ulille/iut/pizzaland/Authentification.java
@@ -1,17 +1,25 @@
 package fr.ulille.iut.pizzaland;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Authentification extends AppCompatActivity {
 
@@ -20,10 +28,15 @@ public class Authentification extends AppCompatActivity {
     public static final String VOLLEY_TAG = "TEST_CHATISFAIT";
     private RequestQueue queue;
 
+    private static String login;
+    private static String mdp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.authent);
+
+        queue = Volley.newRequestQueue(Authentification.this);
     }
 
     public void hideKeyboard() {
@@ -46,39 +59,78 @@ public class Authentification extends AppCompatActivity {
         return host;
     }
 
-    public void showError(VolleyError error) {
-        error.printStackTrace();
+    private String[] cutJsonArray(JSONObject data){
+        String text = data.toString().substring(2, data.toString().length()-1);
+        String splitted[] = text.split(",");
+        //System.out.println(Arrays.deepToString(splitted));
+        return splitted;
     }
 
-    public void showStringResponse(String response) {
+    public void showJsonArrayResponse(JSONArray response) {
+        boolean found = false;
+        try {
+            for (int i = 0; i < response.length() ; i++) {
+                JSONObject obj = response.getJSONObject(i);
+                String[] cut = cutJsonArray(obj);
+                if(cut[2].equals("\"mdp\":\""+mdp+"\"") && cut[3].equals("\"pseudo\":\""+login+"\"") && !found)
+                    found = true;
 
+            }
+
+            String text;
+            int duration = Toast.LENGTH_SHORT;
+
+            text = found ? "Connecté !" : "Login ou mot de passe incorrect";
+
+            Toast toast = Toast.makeText(Authentification.this, text, duration);
+            toast.show();
+
+            //tvDisplay.setText(response.toString(4));
+        } catch (Exception e) {
+            e.printStackTrace();
+            //tvDisplay.setText(getResources().getString(R.string.msg_jsonFormatError));
+        }
     }
 
     public void onConnect(View view){
         hideKeyboard();
+
+        EditText log = (EditText) findViewById(R.id.login);
+        EditText passwd = (EditText) findViewById(R.id.mdp);
+
+        login = log.getText().toString();
+        mdp = passwd.getText().toString();
+
         String base_uri = getFullHostname();
-        System.out.println(base_uri);
         Log.d(LOG_TAG, "Send started");
 
         String uri = base_uri + "/utilisateurs";
-        System.out.println(uri);
+        Log.d(LOG_TAG, "Uri: " + uri);
 
-        /*StringRequest stringRequest = new StringRequest(Request.Method.GET, uri,
-                new Response.Listener<String>() {
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET,
+                uri,
+                null,
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(String response) {
-                        Log.d(LOG_TAG, "Response received" + response);
-                        showStringResponse(response);
+                    public void onResponse(JSONArray response) {
+                        showJsonArrayResponse(response);
+                        //cbGetPizzas.setChecked(true);
                     }
                 },
-                new Response.ErrorListener() {
+                new ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Authentification.this.showError(error);
+                        error.printStackTrace();
                     }
                 });
-        stringRequest.setTag(VOLLEY_TAG);
-        queue.add(stringRequest);
-        Log.d(LOG_TAG, "Send done");*/
+        System.out.println("requete : " + request);
+        request.setTag(VOLLEY_TAG);
+        queue.add(request);
+    }
+
+    public void doInscrip(View view){
+        Intent it = new Intent(Authentification.this, Inscription.class);
+        startActivity(it);
     }
 }
