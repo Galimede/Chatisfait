f26158e693f63c4654074169701656252f88a2b1
Romain Poulain <romain.poulain@etudiant.univ-lille1.fr>
2019-03-21 15:02:20 +0100

    coucou

---
+++ client-js/src/main.js
@@ -0,0 +1,70 @@
+// @flow
+
+import $ from 'jquery';
+import Authent from './pages/Authent.js';
+import AddBox from './pages/AddBox.js';
+import AddAbonnement from './pages/AddAbonnement.js';
+import HomePage from './pages/HomePage.js';
+import PageRenderer from './PageRenderer.js';
+import Menu from './components/Menu.js';
+import AboutUs from './pages/AboutUs.js';
+import ListeArticle from './pages/ListeArticle.js';
+
+
+// configuration du PageRenderer
+PageRenderer.titleElement = document.querySelector('.pageTitle');
+PageRenderer.contentElement = document.querySelector('.contenu');
+
+// Déclaration des pages 
+const homePage:HomePage = new HomePage([]);
+export let authentPage:Authent = new Authent();
+const boxPage:AddBox = new AddBox();
+const abonnementPage:AddAbonnement = new AddAbonnement();
+const aboutUs:AboutUs = new AboutUs();
+const liste:ListeArticle = new ListeArticle();
+
+// Déclaration liens menus 
+const menu:Menu = new Menu();
+const logoLink = $('a.navbar-brand');
+const connexionButton = $('.connecter');
+const abonnementButton = $('.abonner');
+const boxButton = $('.box');
+const aboutButton = $('.about');
+const aboutpanier = $('.panier');
+
+// Gestion des evenements bouttons 
+
+logoLink.click((event:Event) => {
+	event.preventDefault();
+	PageRenderer.renderPage(homePage);
+});
+
+connexionButton.click( (event:Event) => {
+	event.preventDefault();
+	PageRenderer.renderPage(authentPage);
+});
+
+abonnementButton.click( (event:Event) => {
+	event.preventDefault();
+	PageRenderer.renderPage(abonnementPage);
+});
+
+boxButton.click( (event:Event) => {
+	event.preventDefault();
+	PageRenderer.renderPage(boxPage);
+});
+
+aboutButton.click( (event:Event) => {
+	event.preventDefault();
+	PageRenderer.renderPage(aboutUs);
+});
+
+<<<<<<< HEAD
+aboutpanier.click( (event:Event) => {
+	event.preventDefault();
+	PageRenderer.renderPage(liste);
+});
+
+=======
+ 
+>>>>>>> c8c5de438bffeabba560ed00a5a3665bd3355e53
