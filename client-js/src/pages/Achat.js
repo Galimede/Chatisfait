7509da7fd48bf6af05cb7c6179fd5089862f356d
Geldhof Sully <Sully59320@aol.com>
2019-03-25 08:43:42 +0100

    fix

--- client-js/src/pages/Achat.js
+++ client-js/src/pages/Achat.js
@@ -97,8 +97,12 @@ export default class Achat extends Page {
             let htmlcontenu:string="";
             if(panier){
                 panier.forEach( article => { 
+                    console.log(article.idArticle);
+                    if(panier.includes(article.idArticle)){
+                        console.log('TEST')
                         htmlcontenu+= `
                         <li>
+                            <div class="img"><a href=""><img alt="img" src="${article.image}"></a></div>
                             <div class="info">
                                 <a class="nom" href="">${article.nom}</a>
                                 <p>${article.description}</p>
@@ -113,6 +117,7 @@ export default class Achat extends Page {
                             </div>
                         </li>̀`;
                     }
+                });
 
                 $('.liste').html(htmlcontenu);
             }
