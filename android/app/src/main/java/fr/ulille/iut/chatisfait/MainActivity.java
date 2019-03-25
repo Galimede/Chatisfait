4f14f34c7071f7c291b71ec6ed4cd2928130473b
Zinedine ZERROUKI <zinedine.zerrouki.etu@univ-lille.fr>
2019-03-25 09:22:36 +0100

    Debut recherche article par nom --> mobile

--- android/app/src/main/java/fr/ulille/iut/chatisfait/MainActivity.java
+++ android/app/src/main/java/fr/ulille/iut/chatisfait/MainActivity.java
@@ -1,13 +1,12 @@
 package fr.ulille.iut.chatisfait;
 
-import android.app.SearchManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
 
@@ -17,11 +16,16 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements ReceiverClient {
 
     private GenericDataCenter generic;
 
     private String searchQuery;
+
+    private ArrayAdapter adapter;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -33,12 +37,17 @@ public class MainActivity extends AppCompatActivity implements ReceiverClient {
         RequestQueue queue = Volley.newRequestQueue(this);
         generic = new GenericDataCenter(this, queue);
 
+        list = (ListView) findViewById(R.id.list);
+
         search.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
             @Override
             public boolean onQueryTextChange(String text){
                 searchQuery = text;
                 System.out.println(text);
                 generic.doGet(MainActivity.this, GenericDataCenter.Articles);
+                list.setVisibility(searchQuery.length() > 0 ? View.VISIBLE : View.INVISIBLE);
+                //list.setVisibility(View.VISIBLE);
+
                 return true;
             }
 
@@ -52,18 +61,32 @@ public class MainActivity extends AppCompatActivity implements ReceiverClient {
     }
 
     public void getJsonArrayResponse(JSONArray response){
+        ArrayList<String> choix = new ArrayList<>();
+
         try {
             for (int i = 0; i < response.length(); i++) {
                 JSONObject obj = response.getJSONObject(i);
                 String[] cut = generic.cutJsonArray(obj);
                 int nomIdx = generic.searchPattern(cut, ".*nom.*");
                 if(cut[nomIdx].matches(".*"+searchQuery+".*")){
-                    TextView t = (TextView) findViewById(R.id.test);
-                    t.setText(cut[nomIdx]);
+                    choix.add(cut[nomIdx].substring(7,cut[nomIdx].length()-1));
                 }
-                //System.out.println(nomIdx);
+
                 System.out.println(obj);
             }
+            if(choix.size() == 0){
+                choix.add("Aucun résultat");
+            }
+            System.out.println(choix.toString());
+            adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, choix);
+            list.setAdapter(adapter);
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent it = new Intent(MainActivity.this, Article.class);
+                    startActivity(it);
+                }
+            });
         }catch (Exception e){
 
         }
