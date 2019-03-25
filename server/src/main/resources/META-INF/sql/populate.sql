01dcb63274d247b7a6e5c234a1149061eab8988a
Aime MARCANT <aime.marcant@etudiant.univ-lille1.fr>
2019-03-25 10:55:00 +0100

    update article joli 2

--- server/src/main/resources/META-INF/sql/populate.sql
+++ server/src/main/resources/META-INF/sql/populate.sql
@@ -9,10 +9,10 @@ CREATE TABLE commande(idcommande serial NOT NULL PRIMARY KEY, idutilisateur inte
 CREATE TABLE abonnement(idabonnement serial NOT NULL PRIMARY KEY, idutilisateur integer, idarticle integer, age integer, poil char(20), sterilise boolean, poids integer, nom char(20), choix integer, FOREIGN  KEY (idutilisateur) REFERENCES utilisateur(idutilisateur), FOREIGN KEY (idarticle) REFERENCES article(idArticle) ON DELETE CASCADE ON UPDATE CASCADE);
 insert into utilisateur(pseudo,mdp, admin) values ('sacha','sacha',true);
 insert into utilisateur(pseudo,mdp,admin) values ('sully','sully',false);
-insert into article(nom,prix,description,categorie,image) values ('brosse metalique','45','petite brosse pour chat en bois naturel','accessoire','/images/brosse');
-insert into article(nom,prix,description,categorie,image) values ('croquette purizon','20','croquette pour chat','croquette','/images/croquette.png');
-insert into article(nom,prix,description,categorie,image) values ('litiere cat best','10','litiere de qualite cat best original','croquette','/images/litiere');
-insert into article(nom,prix,description,categorie,image) values ('patee purina gourmet','17','patee purina emicee en sauce','croquette','/images/litiere');
+insert into article(nom,prix,description,categorie,image) values ('brosse metalique','45','petite brosse pour chat en bois naturel','accessoire','/images/brosse.jpg');
+insert into article(nom,prix,description,categorie,image) values ('croquette purizon','20','croquette pour chat','croquette','/images/croquette.jpg');
+insert into article(nom,prix,description,categorie,image) values ('litiere cat best','10','litiere de qualite cat best original','croquette','/images/litiere.jpg');
+insert into article(nom,prix,description,categorie,image) values ('patee purina gourmet','17','patee purina emicee en sauce','croquette','/images/litiere.jpg');
 insert into commande(idutilisateur,prix,datecommande,adressemail,adresse,nom,prenom) values ('1','20',' 	2008-10-29' ,'mail@mail.com','77 rue du du','toto','toto');
 insert into abonnement(idutilisateur, idarticle, age, nom) values ('1', '1', '5', 'mistigri');
 insert into note(idarticle,nbnote,notetotale) values (1,10,50);
