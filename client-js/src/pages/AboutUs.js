d9e0e51647374640048b8b99bf0609c1a3488f6c
Ismérie Perrot <perrotismerie@gmail.com>
2019-03-26 10:50:57 +0100

    amélioration aboutUs html css images

--- client-js/src/pages/AboutUs.js
+++ client-js/src/pages/AboutUs.js
@@ -12,9 +12,9 @@ export default class AboutUs extends Page {
         return`
         <section id="banner" class="bannerInOut">
         <div class="banner-bg"></div>
-        <img src="../images/prof2.jpg">
+        <img src="../images/aboutUs.png">
         <div class="container">
-            <p> Chatisfait est une idée de Tom Hazard. Il a souhaité mettre en avant sa passion pour les chats en dédiant toute une gamme de services pour leur bien-être. Que ce soit on en terme d’accessoires, de soins, d’entretien et d’alimentation, mais aussi par la création d’une box avec tous les mois de nouvelles surprises dont ils raffolent. Tout est bon pour chatisfaire votre félin préféré.</p>
+            <h2> Chatisfait est une idée de Tom Hazard. Il a souhaité mettre en avant sa passion pour les chats en dédiant toute une gamme de services pour leur bien-être. Que ce soit on en terme d’accessoires, de soins, d’entretien et d’alimentation, mais aussi par la création d’une box avec tous les mois de nouvelles surprises dont ils raffolent. Tout est bon pour chatisfaire votre félin préféré.</h2>
         </div>
     </section>
 
